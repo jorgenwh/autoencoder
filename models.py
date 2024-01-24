c5bc90eb1c2592054952b5df9d05948bd14700e7
jorgenwh <jorgenwhen@gmail.com>
2024-01-24 21:29:09 +0100

    mlp and conv autoencoders for mnist

---
+++ models.py
@@ -0,0 +1,107 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class MLPEncoder(nn.Module):
+    def __init__(self, input_dim, latent_dim):
+        super(MLPEncoder, self).__init__()
+        self.fc1 = nn.Linear(input_dim, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, latent_dim)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+        x = F.relu(x)
+        return x
+
+
+class MLPDecoder(nn.Module):
+    def __init__(self, latent_dim, output_dim):
+        super(MLPDecoder, self).__init__()
+        self.fc1 = nn.Linear(latent_dim, 128)
+        self.fc2 = nn.Linear(128, 256)
+        self.fc3 = nn.Linear(256, output_dim)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+        x = F.sigmoid(x)
+        return x
+
+
+class MLPAutoEncoder(nn.Module):
+    def __init__(self, input_dim=784, latent_dim=3):
+        super(MLPAutoEncoder, self).__init__()
+        self.encoder = MLPEncoder(input_dim, latent_dim)
+        self.decoder = MLPDecoder(latent_dim, input_dim)
+
+    def encode(self, x):
+        return self.encoder(x)
+
+    def decode(self, x):
+        return self.decoder(x)
+
+    def forward(self, x):
+        x = self.encode(x)
+        x = self.decode(x)
+        return x
+
+
+class ConvEncoder(nn.Module):
+    def __init__(self):
+        super(ConvEncoder, self).__init__()
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(16, 32, kernel_size=7, stride=1, padding=0)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.conv3(x)
+        x = F.relu(x)
+        return x
+
+
+class ConvDecoder(nn.Module):
+    def __init__(self):
+        super(ConvDecoder, self).__init__()
+        self.conv1 = nn.ConvTranspose2d(32, 16, kernel_size=7, stride=1, padding=0)
+        self.conv2 = nn.ConvTranspose2d(16, 8, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.conv3 = nn.ConvTranspose2d(8, 1, kernel_size=3, stride=2, padding=1, output_padding=1)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.conv3(x)
+        x = F.sigmoid(x)
+        return x
+
+
+class ConvAutoEncoder(nn.Module):
+    def __init__(self):
+        super(ConvAutoEncoder, self).__init__()
+        self.encoder = ConvEncoder()
+        self.decoder = ConvDecoder()
+
+    def encode(self, x):
+        return self.encoder(x)
+
+    def decode(self, x):
+        return self.decoder(x)
+
+    def forward(self, x):
+        x = self.encode(x)
+        x = self.decode(x)
+        return x
